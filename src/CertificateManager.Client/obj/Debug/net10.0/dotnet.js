260b7c0624dcf9490ceb047cd813b037cb308bc9
Samuel Carson <samuel.carson@gmail.com>
2025-06-04 01:29:06 -0500

    Updated avMenu.razor file with the link to the new "Certificates" page.
    Summary of Client-Side Changes for Displaying Certificates:
    
    Shared Models: CertificateManager.Shared project created and integrated.
    Certificate Service: ICertificateService and CertificateService updated with GetAllCertificatesAsync.
    Blazor Component: CertificateList.razor created to fetch and display certificates.
    Navigation: Link added to NavMenu.razor.

--- src/CertificateManager.Client/obj/Debug/net10.0/dotnet.js
+++ src/CertificateManager.Client/obj/Debug/net10.0/dotnet.js
@@ -4,7 +4,7 @@ var e=!1;const t=async()=>WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,
   "mainAssemblyName": "CertificateManager.Client",
   "applicationEnvironment": "Development",
   "resources": {
-    "hash": "sha256-EYgoregmDS0OP0aaSgGnR1SJw6oBe/gLFpExm14s+3k=",
+    "hash": "sha256-Ze/CnRgPaNtzPPowFT+1CR2A4M+7I6TFY1cSiom4pCo=",
     "fingerprinting": {
       "Microsoft.AspNetCore.Authorization.xzblanle4u.wasm": "Microsoft.AspNetCore.Authorization.wasm",
       "Microsoft.AspNetCore.Components.yl5qce0m75.wasm": "Microsoft.AspNetCore.Components.wasm",
@@ -211,10 +211,10 @@ var e=!1;const t=async()=>WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,
       "icudt_CJK.tjcz0u77k5.dat": "icudt_CJK.dat",
       "icudt_EFIGS.tptq2av103.dat": "icudt_EFIGS.dat",
       "icudt_no_CJK.lfu7j35m59.dat": "icudt_no_CJK.dat",
-      "CertificateManager.Shared.jkewesuslb.wasm": "CertificateManager.Shared.wasm",
-      "CertificateManager.Shared.g4sierqr7m.pdb": "CertificateManager.Shared.pdb",
-      "CertificateManager.Client.feem6ofs76.wasm": "CertificateManager.Client.wasm",
-      "CertificateManager.Client.vqt66rdla0.pdb": "CertificateManager.Client.pdb"
+      "CertificateManager.Shared.k90awjsxr9.wasm": "CertificateManager.Shared.wasm",
+      "CertificateManager.Shared.nhgqll83wc.pdb": "CertificateManager.Shared.pdb",
+      "CertificateManager.Client.bi6oz1fzzr.wasm": "CertificateManager.Client.wasm",
+      "CertificateManager.Client.790jprg2bl.pdb": "CertificateManager.Client.pdb"
     },
     "jsModuleNative": {
       "dotnet.native.ag7edw449v.js": "sha256-xvVp/fZ4Y6bUzf5qhusfIdl1iBnAHjDV8xQ/ELYPXMs="
@@ -432,12 +432,12 @@ var e=!1;const t=async()=>WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,
       "WindowsBase.x8kacodv3x.wasm": "sha256-AQFfd/tAEfdj9Zda1OnOLMOi8x3xF3D0DL3fmA9FeP4=",
       "mscorlib.vqgsl7wzlm.wasm": "sha256-OUsFFZqlOqzu0vx7zY3cIOhACYsV4ensuX6qeIaM6Pk=",
       "netstandard.pa5mbjs6mr.wasm": "sha256-r+Qu6UdaWsqkiwAC5o5/kuJtQ4pi1nb9Hwo9cJtDoxc=",
-      "CertificateManager.Shared.jkewesuslb.wasm": "sha256-PVC0GTh/D3u5BNzIIIfDnA5TO7OE3T4ULRFd8m1XDCU=",
-      "CertificateManager.Client.feem6ofs76.wasm": "sha256-mfdmZv/bjtDBN2FBeOgvqvZGH2RwREWTIg+wSthkY84="
+      "CertificateManager.Shared.k90awjsxr9.wasm": "sha256-2GswVC6mnGlzr6kQbDNDYLNs/R7yyco2qgaI4CowTU8=",
+      "CertificateManager.Client.bi6oz1fzzr.wasm": "sha256-843wtbDDcTRQBzTzQ0Hyy8MmfCB5S/Csng13Jg087is="
     },
     "pdb": {
-      "CertificateManager.Shared.g4sierqr7m.pdb": "sha256-4NiadhihpnVF1xegEZX465NxBi4HqAwaAF0+jdPG6qY=",
-      "CertificateManager.Client.vqt66rdla0.pdb": "sha256-6QFJPX/OqAnERRp9QoZiT/lmdfFEPvXEGhIvxMHrefk="
+      "CertificateManager.Shared.nhgqll83wc.pdb": "sha256-+75/R5dXXr4gZfH4r897xQ1wvZQ6TWj2kIQlIrw91Bw=",
+      "CertificateManager.Client.790jprg2bl.pdb": "sha256-Cw3KH2J04uFUq97bgoqK0ztRN2YRs9i0BZt33SubSpY="
     }
   },
   "cacheBootResources": true,
--- src/CertificateManager.Shared/obj/Debug/net10.0/CertificateManager.Shared.AssemblyInfo.cs
+++ src/CertificateManager.Shared/obj/Debug/net10.0/CertificateManager.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CertificateManager.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+838e5e1db927f54bf2f20f386b0479af5f395748")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10b8b841c7bc456945d128ace178b6178dec05bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("CertificateManager.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CertificateManager.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
